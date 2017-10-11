1584f4b624e47eefb7c735d3585c69025b13ab55
刘祥洋 <xiangyang.liu@mljr.com>
2017-10-11 16:41:35 +0800

    第一次提交

---
+++ LxyTestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PodDemo
+//
+//  Created by lxy on 2017/10/11.
+//  Copyright © 2017年 lxy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ PodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodDemo
+//
+//  Created by lxy on 2017/10/11.
+//  Copyright © 2017年 lxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
